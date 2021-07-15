e4f5c80a6696d603115b625435dd1d67976d9d9b
Sam Miladian <miladian.samad@gmail.com>
2021-07-16 02:57:54 +0430

    create a basic login and starting the TM

--- app.py
+++ app.py
@@ -1,27 +1,77 @@
-# from flask import Flask, render_template
-import flask
-# from flask import Flask
-app = flask.Flask(__name__)
+from flask import Flask, render_template, Response, redirect, url_for, request, session, abort
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+import config
+app = Flask(__name__)
 
-@app.route("/")
-def welcome():
-    return flask.render_template('index.html')
+app.config.update(
+    SECRET_KEY = config.SECRET_KEY
+)
 
-@app.route("/login", methods=['GET', 'POST'])
+# flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+# silly user model
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        
+    def __repr__(self):
+        return "%d" % (self.id)
+
+
+# create some users with ids 1 to 20       
+user = User(0)
+
+# some protected url
+@app.route('/')
+@login_required
+def home():
+    return render_template("index.html")
+
+ 
+# somewhere to login
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginFrom()
-    if form.validate_on_submit():
-        # Login and validate the user.
-        # user should be an instance of your `User` class
-        login_user(user)
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']        
+        if password == config.PASSWORD and username == config.USERNAME:
+            login_user(user)
+            return redirect("/TM")
+            # return redirect(request.args.get("next"))
+        else:
+            return abort(401)
+    else:
+        return render_template("login.html")
+        
+# somewhere to logout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return render_template("logout.html")
 
-        flask.flash('Logged in successfully.')
 
-        next = flask.request.args.get('next')
-        # is_safe_url should check if the url is safe for redirects.
-        # See http://flask.pocoo.org/snippets/62/ for an example.
-        if not is_safe_url(next):
-            return flask.abort(400)
+# handle login failed
+@app.errorhandler(401)
+def page_not_found(error):
+    return render_template("erorr.html")
+    # return Response('<p>Login failed</p>')
+    
+    
+# callback to reload the user object        
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
 
-        return flask.redirect(next or flask.url_for('index'))
-    return flask.render_template('login.html', form=form)
+@app.route("/TM")
+@login_required
+def TM():
+    return render_template("TM.html")
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000, debug=True)
---
+++ config.py
@@ -0,0 +1,3 @@
+SECRET_KEY = "_MbY--f9^n@S}s&+Aj*[r]`c!oT_Q[B1$z9{@qS-)qsA*lNN[}"
+USERNAME = "Sami"
+PASSWORD = "samuel"
